7ba37f8ffaffc702da143acdfe81d5d6fd082069
Mohamed <kaytoon23@gmail.comd>
2017-12-03 00:28:33 -0800

    script updated for automation added

--- swaphz-build-lambda.py
+++ swaphz-build-lambda.py
@@ -2,16 +2,18 @@ import boto3
 import StringIO
 import zipfile
 import mimetypes
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-swaphz_bucket = s3.Bucket('swaphz')
-build_bucket = s3.Bucket('build-swaphz')
+    swaphz_bucket = s3.Bucket('swaphz')
+    build_bucket = s3.Bucket('build-swaphz')
 
-swaphz_zip = StringIO.StringIO()  
-build_bucket.download_fileobj('swaphzbuild.zip', swaphz_zip)  
+    swaphz_zip = StringIO.StringIO()  
+    build_bucket.download_fileobj('swaphzbuild.zip', swaphz_zip)  
 
-with zipfile.ZipFile(swaphz_zip) as myzip:  
-     for nm in myzip.namelist():  
-        obj = myzip.open(nm)
-        swaphz_bucket.upload_fileobj(obj,nm,ExtraArgs ={'ContentType': mimetypes.guess_type(nm)[0]})
-        swaphz_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(swaphz_zip) as myzip:  
+        for nm in myzip.namelist():  
+            obj = myzip.open(nm)
+            swaphz_bucket.upload_fileobj(obj,nm,ExtraArgs ={'ContentType': mimetypes.guess_type(nm)[0]})
+            swaphz_bucket.Object(nm).Acl().put(ACL='public-read')
+    return 'Hello from Lambda'
